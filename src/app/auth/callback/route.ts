1c5c4a0637121ed8833ba4dd4d437ad6bf4e85a6
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-06-14 22:41:59 -0400

    fix?

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
     // if "next" is in param, use it as the redirect URL
-    const next = searchParams.get('next') ?? '/home/dashboard'
+    const next = '/home/dashboard'
 
     if (code) {
         const cookieStore = cookies()
